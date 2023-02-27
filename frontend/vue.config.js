187b545b0a766bbafcbad4ede01d7688abab831a
CONTINUE <sghan811@gmail.com>
2023-02-27 14:49:57 +0900

    host

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -3,13 +3,13 @@ var astore = require('./src/vuex/store')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-  disableHostCheck: true,
   devServer: {
     proxy: {
       '/api': {
         target: 'http://127.0.0.1:3000/',
         changeOrigin: true
       }
-    }
+    },
+    allowedHosts: "all",
   },
 })
